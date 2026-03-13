a429087d196edddee66553545121680a2e24b9a5
ItsKareem <1022027.kareem@stemegypt.edu.eg>
2026-03-13 16:09:07 +0200

    better file names and formatting because I got cooked by my friends

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // FUN FACT BUTTON
 const allFacts = [
-  "My MBTI is INFP!",
-  "I make stickers for a bunch of events! Ask me for one next time we meet! :3",
-  "I know Arabic, English, high school French, and am trying to learn Mandarin! (HSK1 tho)",
-  "I LOVE LOVE LOVE FOOD.",
-  "I am a very bad liar.",
-  "I have a VERY large plushie collection (more than an 18-year-old probably should)",
-  "As a kid, my internet alias was “Luigi”.",
-  "I sometimes spell my name in the wrong way “Kareem” because that’s what I’m used to.",
-  "I wear headphones almost all the time."
+    "My MBTI is INFP!",
+    "I make stickers for a bunch of events! Ask me for one next time we meet! :3",
+    "I know Arabic, English, high school French, and am trying to learn Mandarin! (HSK1 tho)",
+    "I LOVE LOVE LOVE FOOD.",
+    "I am a very bad liar.",
+    "I have a VERY large plushie collection (more than an 18-year-old probably should)",
+    "As a kid, my internet alias was “Luigi”.",
+    "I sometimes spell my name in the wrong way “Kareem” because that’s what I’m used to.",
+    "I wear headphones almost all the time."
 ];
 
-let availableFacts = [...allFacts]; 
+let availableFacts = [...allFacts];
 
 const rabbotButton = document.getElementById('rab-bot-button');
 const factDisplay = document.getElementById('fact-text');
 
 rabbotButton.addEventListener('click', () => {
-  if (availableFacts.length === 0) {
-    availableFacts = [...allFacts];
-  }
+    if (availableFacts.length === 0) {
+        availableFacts = [...allFacts];
+    }
 
-  const randomIndex = Math.floor(Math.random() * availableFacts.length);
-  
-  const chosenFact = availableFacts.splice(randomIndex, 1);
+    const randomIndex = Math.floor(Math.random() * availableFacts.length);
 
-  factDisplay.innerText = chosenFact;
-  rabbotButton.src = "images/Rab-bot-pressed.webp";
+    const chosenFact = availableFacts.splice(randomIndex, 1);
 
-setTimeout(() => {rabbotButton.src = "images/Rab-bot.webp";}, 500);
+    factDisplay.innerText = chosenFact;
+    rabbotButton.src = "images/Rab-bot-pressed.webp";
+
+    setTimeout(() => { rabbotButton.src = "images/Rab-bot.webp"; }, 500);
 
 });
